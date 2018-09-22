5f3a58670b2a94b0d8e07c638738802343f67d73
quasim <nicomaxa@gmail.com>
2018-09-22 15:59:00 +0200

    Chapter05 Creating a plugin & Plugin options

--- support-center/src/main.js
+++ support-center/src/main.js
@@ -3,10 +3,15 @@ import Vue from 'vue'
 import AppLayout from './components/AppLayout.vue'
 import router from './router'
 import './global-components'
+import VueFetch from './plugins/fetch'
 
+Vue.use(VueFetch,{
+  baseUrl: 'http://localhost:3000/'
+})
 new Vue({
   el: '#app',
   render: h => h(AppLayout),
 
   router,
+
 })
