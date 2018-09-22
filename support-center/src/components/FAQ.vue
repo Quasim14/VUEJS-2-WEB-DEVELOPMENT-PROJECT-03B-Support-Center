429b6642a53079adb0e675cb04a70120e03be8a0
quasim <nicomaxa@gmail.com>
2018-09-22 15:20:19 +0200

    Chapter05 Server setup & Using fetch

--- support-center/src/components/FAQ.vue
+++ support-center/src/components/FAQ.vue
@@ -1,13 +1,59 @@
 <template>
   <main class="faq">
     <h1>Frenquently Asked Questions</h1>
+
+    <div class="error" v-if="error">
+      Can't load the questions
+    </div>
+
+    <section class="list">
+      <article v-for="question of questions">
+        <h2 v-html="question.title"></h2>
+        <p v-html="question.content"></p>
+      </article>
+    </section>
+
   </main>
 
 </template>
 
 <script>
     export default {
-        name: "Faq"
+      name: "Faq",
+      data(){
+          return{
+            questions:[],
+            error:null,
+          }
+      },
+      created(){
+        fetch('http://localhost:3000/questions').then(response =>{
+          if(response.ok){
+            return response.json()
+          }
+          else{
+            return Promise.reject('error')
+          }
+        }).then(result =>{
+          this.questions = result
+        }).catch(e =>{
+          this.error = e
+        })
+      },
+      async created(){
+        try{
+          const response = await fetch('http://localhost:3000/questions')
+          if(response.ok){
+            this.questions = await response.json()
+          }
+          else{
+            throw new Error('error')
+          }
+        }
+        catch(e){
+          this.error = e
+        }
+      },
     }
 </script>
 
