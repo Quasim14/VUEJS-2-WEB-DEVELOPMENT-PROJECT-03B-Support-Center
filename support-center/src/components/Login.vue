2550a801a94da29b128e08a2bb118359d150dd84
quasim <nicomaxa@gmail.com>
2018-09-23 14:13:13 +0200

    Chapter05 Form input component

--- support-center/src/components/Login.vue
+++ support-center/src/components/Login.vue
@@ -1,10 +1,29 @@
 <template>
+  <main class="login">
+    <h1>Please login to continue</h1>
+    <form>
+      <FormInput name="username" v-model="username" placeholder="Username" />
+    </form>
+  </main>
 
 </template>
 
 <script>
     export default {
-        name: "Login"
+      name: "Login",
+
+      data(){
+        return {
+          username: '',
+        }
+      },
+      methods:{
+        update(event){
+          this.$emit('input', event.currentTarget.value)
+        },
+      },
+
+
     }
 </script>
 
