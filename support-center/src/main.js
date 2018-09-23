717ce03af74e6a015c80248c5a2fa95ca6335d6e
quasim <nicomaxa@gmail.com>
2018-09-23 13:01:40 +0200

    Chapter05 User authentication

--- support-center/src/main.js
+++ support-center/src/main.js
@@ -4,14 +4,17 @@ import AppLayout from './components/AppLayout.vue'
 import router from './router'
 import './global-components'
 import VueFetch from './plugins/fetch'
+import state from './state'
+import VueState from './plugins/state'
 
 Vue.use(VueFetch,{
   baseUrl: 'http://localhost:3000/'
 })
 new Vue({
   el: '#app',
+  data: state,
+  router,
   render: h => h(AppLayout),
 
-  router,
 
 })
