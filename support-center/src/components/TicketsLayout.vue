2aadd847590be1b05cd737f10563f367fae2a33e
quasim <nicomaxa@gmail.com>
2018-09-25 12:09:56 +0200

    Chapter05 Route meta properties.

---
+++ support-center/src/components/TicketsLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="tickets-layout">
+    <h1>Your Support tickets</h1>
+    <!-- TODO -->
+  </main>
+
+</template>
+
+<script>
+    export default {
+        name: "TicketsLayout"
+    }
+</script>
+
+<style scoped>
+
+</style>
